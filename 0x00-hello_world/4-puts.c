210155ba75621c5bc1c5cf1fa4ddffa9a626a6fc
Prince-ij <aminuibrahimthefifth@gmail.com>
2022-10-13 00:52:58 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-	int main (void)
+	/** is it by 
+	 * force to 
+	 * write documentation.
+	 * make una allow man rest now
+	 */
+	int main(void)
 {	
 	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
