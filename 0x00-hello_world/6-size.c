6b7fa0916f8365a1322719c370582960554d6c27
Prince-ij <aminuibrahimthefifth@gmail.com>
2022-10-13 01:45:24 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry point
  *
- * Return : Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -16,6 +16,6 @@ int main(void)
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("Sizze of a float: %lu  byte(s)\n", (unsigned long)sizeof(e));
+	printf("Size of a float: %lu  byte(s)\n", (unsigned long)sizeof(e));
 	return (0);
 }
