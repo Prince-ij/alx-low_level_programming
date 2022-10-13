bb84a90ca0cf4fa9da12f3566ba7b98289a4e752
Prince-ij <aminuibrahimthefifth@gmail.com>
2022-10-13 01:15:05 -0700

    t5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-int main(){
+#include <stdio.h>
+/*
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
