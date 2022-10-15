e34edd5ac8d45e0fd72662e8eec1a6b70a1dd84e
Prince-ij <aminuibrahimthefifth@gmail.com>
2022-10-15 14:28:00 -0700

    sugar

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'task 4'
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+	int z;
+
+	while (a <= z)
+	{
+		if (a != 'q' && a != 'e')
+		{
+			putchar(a);
+		}
+	}
+	return (0);
+}
