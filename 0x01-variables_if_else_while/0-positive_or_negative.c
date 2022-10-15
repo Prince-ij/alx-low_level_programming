7e7c3a4ecfe0d93f82f865590ddc283258d4cfaf
Prince-ij <aminuibrahimthefifth@gmail.com>
2022-10-15 05:30:58 -0700

    fic

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,7 +11,6 @@
 int main(void)
 {
 	int n;
-	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
@@ -25,6 +24,6 @@ int main(void)
 	else
 	{
 		printf("%d is negative\n", n);
-	}
+	
 	return (0);
 }
