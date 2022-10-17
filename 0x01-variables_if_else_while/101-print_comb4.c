1ba9a57f837699aec485675c5194659d7bc4bc7a
Prince-ij <aminuibrahimthefifth@gmail.com>
2022-10-17 11:23:04 -0700

    hjkiolpbjbdsjbfakbslhblav

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,14 +16,14 @@ int main(void)
 			{
 				if (c < i && i < k)
 				{
-																putchar(c);
-																putchar(i);
-																putchar(k);
-																if (c != '7')
-																{
-																	putchar(',');
-																	putchar(' ');
-																}
+					putchar(c);
+					putchar(i);
+					putchar(k);
+					if (c != 7)
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
 			}
 		}
