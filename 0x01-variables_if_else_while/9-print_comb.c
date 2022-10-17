bb48a7434e2b6431e3a60ea627ed821364b840f1
Prince-ij <aminuibrahimthefifth@gmail.com>
2022-10-17 06:45:22 -0700

    k

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- *  * main - Entry point
- *   * Description: prints numbers
- *    * Return: Always 0 (success)
- *     */
+ * main - Entry point
+ * Description: prints numbers
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-		int i;
+	int i;
 
-			for (i = '0'; i <= '9' ; i++)
-					{
-								putchar(i);
+	for (i = '0'; i <= '9' ; i++)
+	{
+		putchar(i);
 
-										if (i != '9')
-													{
-																	putchar(',');
-																				putchar(' ');
-																						}
-											}
-
-				putchar('\n');
-					return (0);
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
