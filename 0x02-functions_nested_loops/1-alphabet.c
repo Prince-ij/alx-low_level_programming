40cf94ee4053e3d3cf2a4549f36263d77469fb42
Prince-ij <aminuibrahimthefifth@gmail.com>
2022-10-18 05:59:18 -0700

    added files

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: blah blah blah
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	for (int a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+}
+int main(void)
+{
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry poinr
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	for (i = 1; i <= 10; i++)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			putchar(a);
+		}
+		putchar('\n');
+	}
+}
+int main(void)
+{
+	return (0);
+}
