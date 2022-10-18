d639d9bbdab86fd2ab448301ddcd42f080f134e2
Prince-ij <aminuibrahimthefifth@gmail.com>
2022-10-18 05:25:31 -0700

    tsk 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: First task
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
