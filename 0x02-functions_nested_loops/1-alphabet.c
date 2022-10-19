eea4ca431af68fb270fc1d1f150779896b5a6d70
Prince-ij <aminuibrahimthefifth@gmail.com>
2022-10-18 21:43:54 -0700

    j

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,8 +3,8 @@
 /**
  * main - Entry point
  *
- * Description: blah blah blah
- * Return: Always 0
+ * Description: 'print alphabet'
+ * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - Entry poinr
+ * main - Entry point
  *
  * Return: 0
  */
 void print_alphabet_x10(void)
 {
 	int a, i;
+
 	for (i = 1; i <= 10; i++)
 	{
 		for (a = 'a'; a <= 'z'; a++)
