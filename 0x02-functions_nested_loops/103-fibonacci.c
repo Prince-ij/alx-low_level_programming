9fea15efb5b9539f54c569172af4781df6b2d35f
Prince-ij <aminuibrahimthefifth@gmail.com>
2022-10-19 21:53:06 -0700

    rattatyouile

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * *main - prints sum of even fibonacci
- * *suit elements
- * *fibonacci suit numbers
- * *Return: return 0
- * */
+ * main - prints sum of even fibonacci
+ * suit elements
+ * fibonacci suit numbers
+ * Return: return 0
+ */
 int main(void)
 {
 	unsigned long n1 = 0, n2 = 1, n3 = 0, sum = 0;
--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * *print_to_98 - prints natural
- * *numbers from n to 98
- * *@n:integer to start by
- * *return: returns nothing
- * */
+ * print_to_98 - prints natural
+ * numbers from n to 98
+ * @n:integer to start by
+ * Return: returns nothing
+ */
 void print_to_98(int n)
 {
 	if (n >= 98)
