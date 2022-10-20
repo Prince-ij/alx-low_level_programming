25b3ad7c37a6608a781905d1bf535753502eab2e
Prince-ij <aminuibrahimthefifth@gmail.com>
2022-10-20 03:08:35 -0700

    %

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int largest_number(int, int, int);
+int largest_number(int, int, int)i;
 void print_remaining_days(void);
+int convert_day(int, int);
 #endif /* MAIN_H */
