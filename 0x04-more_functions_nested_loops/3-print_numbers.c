508167fe6a3f20ec4384d37455bc7fcdc74fa067
Prince-ij <aminuibrahimthefifth@gmail.com>
2022-10-20 08:42:52 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - Entry point
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+	
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
