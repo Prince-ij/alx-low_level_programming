b6878be2abd9a5c6856930f393af7aca4c90358b
Prince-ij <aminuibrahimthefifth@gmail.com>
2022-10-20 23:10:16 -0700

    dff

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - Entry point
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int r, i;
+
+	for (r = 0; r <= 10; r++)
+	{
+	for (i = 0; i <= 14; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	}
+}
