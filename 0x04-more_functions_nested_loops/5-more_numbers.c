fc3ddf55a106620f51611ac3225d72741003015d
Prince-ij <aminuibrahimthefifth@gmail.com>
2022-10-21 13:47:33 -0700

    v

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,14 +6,22 @@
  */
 void more_numbers(void)
 {
-	int r, i;
+	int n, c, i;
 
-	for (r = 0; r <= 10; r++)
+	i = 0;
+	while (i < 10)
 	{
-	for (i = 0; i <= 14; i++)
-	{
-		_putchar(i + '0');
-	}
-	_putchar('\n');
-	}
+		for (n = 0; n <= 14; n++)
+		{
+			c = n;
+			if (n > 9)
+			{
+				_putchar('1');
+				c = n % 10;
+			}
+			_putchar('0' + c);
+		}
+		_putchar('\n');
+		i++;
 }
+
