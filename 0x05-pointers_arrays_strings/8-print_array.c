ce0e7ae85dc313774465d1ce81bec5cfe745e93c
Prince-ij <aminuibrahimthefifth@gmail.com>
2022-10-25 03:13:09 -0700

    &66

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,11 +9,13 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	i = 0;
-	for (n--; n >= 0; n--, i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		printf(", ");
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
 	}
 	printf("\n");
 }
