aa53646b04678ade4743741fd200390c2f9eb7e8
Prince-ij <aminuibrahimthefifth@gmail.com>
2022-11-09 15:53:19 -0800

    I did my best

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * create_array - gjfsd hjjx hj
+ * @size: uizfj
+ * @c: hjdf
+ * Return: pufbahjd
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int index;
+
+	if (size == 0)
+		return (NULL);
+	array = malloc(sizeof(char) * size);
+	if (array == NULL)
+		return (NULL);
+
+	for (index = 0; index < size; index++)
+		array[index] = c;
+	return (array);
+}
