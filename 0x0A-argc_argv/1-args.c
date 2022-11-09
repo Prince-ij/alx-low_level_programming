b0a3d5e3b142fc877d0dbea12c84d95e92d62798
Prince-ij <aminuibrahimthefifth@gmail.com>
2022-11-09 14:57:58 -0800

    ij

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: param
+ * @argv: param
+ * Return: Always 0 Success
+ */
+int main(int argc, char __attribute__((__unused__)) **argv)
+{
+	printf("%i\n", argc - 1);
+
+	return (0);
+}
