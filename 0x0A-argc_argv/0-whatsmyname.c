5f964eb7ce3be596b16d18a03afd111ac811b1ed
Prince-ij <aminuibrahimthefifth@gmail.com>
2022-11-09 14:31:50 -0800

    first task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 Success
+ */
+int main(int argc, char **argv)
+{
+	printf("%s", argv[0]);
+	return (0);
+}
