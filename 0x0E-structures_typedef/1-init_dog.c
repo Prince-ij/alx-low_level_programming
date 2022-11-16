8a84e96cd071acbdee8db2b3c6174b9d99ddf52c
Prince-ij <aminuibrahimthefifth@gmail.com>
2022-11-15 18:48:55 -0800

    secind pish

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+/**
+ * init_dog - function that initializes
+ * @name: param
+ * @owner: param
+ * @age: param
+ * @d: param
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d.name->name;
+		d.age->age;
+		d.owner->owner;
+	}
+}
