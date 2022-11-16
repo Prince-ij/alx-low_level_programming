f3c25ddbdf822af82d479a790778ef8d290df68a
Prince-ij <aminuibrahimthefifth@gmail.com>
2022-11-15 18:11:17 -0800

    first push

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * struct dog - Struct definition
+ * @name: char
+ * @age: decimal
+ * @owner: char
+ * Description: a struct defined to contain the above argumnts
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
