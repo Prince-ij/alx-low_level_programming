5a2f007223e7e5051c8815efa036c20cd99b94c6
Prince-ij <aminuibrahimthefifth@gmail.com>
2022-11-15 20:58:30 -0800

    677

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,4 @@
-#ifndef ABS(X)
-#define ABS(X) (X < 0) ? -(X) : (X)
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+#define ABS(X) ((X < 0) ? (X) * (-1) : (X))
 #endif
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,3 +1,5 @@
-#ifndef SUM(x, y)
+#ifndef SUM_H
+#define SUM_H
+
 #define SUM(x, y) ((x) + (y))
 #endif
