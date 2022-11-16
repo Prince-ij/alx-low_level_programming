f85dba4f27282eb9e180b20b3836aab75f901420
Prince-ij <aminuibrahimthefifth@gmail.com>
2022-11-15 19:42:12 -0800

    eveting

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,42 @@
+#include "string.h"
+/**
+ * new_dog - function
+ * @name: paeam
+ * @age: param
+ * @owner: para.
+ * Return: void
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dogg;
+
+	if (name == NULL || age < 0 || owner == NULl)
+		return (NULL);
+	dogg = malloc(sizeof(dog_t));
+	if (dogg == NULL)
+	{
+		free(dogg);
+		return (NULL);
+	}
+
+	dogg->name = malloc(sizeof(char) * strlen(name + 1));
+	if (dogg->name == NULl)
+	{
+		free(dogg);
+		return (NULL);
+	}
+
+	dogg->owner = malloc(sizeof(char) * strlen(owner + 1));
+	if (dog->owner == NULL)
+	{
+		free(dogg->name);
+		free(dogg);
+		return (NULL);
+	}
+
+	strcpy(dogg->name, name);
+	dogg->age = age;
+	strcpy(dogg->owner, owner);
+
+	return (dogg);
+}
