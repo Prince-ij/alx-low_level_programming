7ca0d838d6f2cea4942460e84d8c638216a754b5
Prince-ij <aminuibrahimthefifth@gmail.com>
2022-11-15 18:35:22 -0800

    second push

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,8 @@
+#ifndef OG_H
+#define OG_H
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * struct dog - Struct definition
  * @name: char
@@ -12,3 +16,13 @@ struct dog
 	float age;
 	char *owner;
 };
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
