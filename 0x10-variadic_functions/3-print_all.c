6158141544b2ddecb44bfc4015a6ac9dab9b7da4
Prince-ij <aminuibrahimthefifth@gmail.com>
2022-11-18 17:55:54 -0800

    *__________________*

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -50,13 +50,14 @@ void print_string(va_list arg)
 }
 /**
  * print_all - func
- * @arg: param
+ * @format: param
  */
 void print_all(const char * const format, ...)
 {
 	va_list args;
 
-	int i = 0; int j = 0;
+	int i = 0;
+	int j = 0;
 
 	char *separator = "";
 
