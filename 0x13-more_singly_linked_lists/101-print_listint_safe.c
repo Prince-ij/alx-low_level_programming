4385bb854c8dc0ed615e9ba615629d4e82691329
Prince-ij <aminuibrahimthefifth@gmail.com>
2022-11-30 12:52:30 -0800

    45689

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -15,7 +15,7 @@ size_t looped_listint_len(const listint_t *head)
 	const listint_t *tortoise, *hare;
 	size_t nodes = 1;
 
-	if (head == NULL || head->next == NUL)
+	if (head == NULL || head->next == NULL)
 		return (0);
 	tortoise = head->next;
 	hare = (head->next)->next;
