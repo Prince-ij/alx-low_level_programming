889871c6ea13f70f8b0986f5de9bac318713e561
Prince-ij <aminuibrahimthefifth@gmail.com>
2022-11-30 13:37:26 -0800

    **kk**

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,34 +1,83 @@
 #include "lists.h"
-#include <stdio.h>
+
+size_t looped_listint_count(listint_t *head);
+size_t free_listint_safe(listint_t **h);
+
+/**
+ * looped_listint_count - function
+ * @head: param
+ * Return: 0
+ */
+
+size_t looped_listint_count(listint_t *head)
+{
+	listint_t *tortoise, *hare;
+	size_t nodes = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+	tortoise = head->next;
+	hare = (head->next)->next;
+
+	while (hare)
+	{
+		if (tortoise == hare)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+			tortoise = tortoise->next;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+			}
+			return (nodes);
+		}
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
+	}
+	return (0);
+}
+
 /**
  * free_listint_safe - function
  * @h: param
- * Return: void
+ * Return: null
  */
+
 size_t free_listint_safe(listint_t **h)
 {
-	size_t len = 0;
-	int diff;
-	listint_t *temp;
+	listint_t *tmp;
+	size_t nodes, index;
 
-	if (!h || !*h)
-		return (0);
-	while (*h)
+	nodes = looped_listint_count(*h);
+
+	if (nodes == 0)
 	{
-		diff = *h - (*h)->next;
-		if (diff > 0)
+		for (; h != NULL && *h != NULL; nodes++)
 		{
-			temp = (*h)->next;
-			*h = temp;
-			len++;
-		}
-		else
-		{
-			*h = NULL;
-			len++;
-			break;
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
 		}
 	}
-	*h = NULL;
-	return (len);
+	else
+	{
+		for (index = 0; index < nodes; index++)
+		{
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
+		}
+
+		*h = NULL;
+
+	}
+	h = NULL;
+	return (nodes);
 }
