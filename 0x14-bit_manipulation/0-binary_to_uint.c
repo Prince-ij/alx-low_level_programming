f49667431279d3913a5b59da51958d376636d9fc
Prince-ij <aminuibrahimthefifth@gmail.com>
2022-12-01 12:34:55 -0800

    karibu

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,40 +7,16 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int decimal = 0;
-	int str_len = 0, base = 1;
+	int i;
+	unsigned int dec_val = 0;
 
-	if (!check_valid_string(b))
+	if (!b)
 		return (0);
-
-	while (b[str_len] != '\0')
-		str_len++;
-
-	while (str_len)
+	for (i = 0; b[i]; i++)
 	{
-		decimal += ((b[str_len - 1] - '0') * base);
-		base *= 2;
-		str_len--;
-	}
-	return (0);
-}
-
-/**
- * check_valid_string - helper functikn
- * @b: param
- * Return: void
- */
-
-int check_valid_string(const char *b)
-{
-	if (b == NULL)
-		return (0);
-
-	while (*b)
-	{
-		if (*b != '1' && *b != '0')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		b++;
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
-	return (1);
+	return (dec_val);
 }
