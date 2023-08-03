5944af1d45499851b65b39fac4ee419ed48acdcf
Prince-ij <aminuibrahimtbefifth@gmail.com>
2023-08-03 09:14:50 -0700

    03-08-23

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,12 +1,11 @@
-#ifndef "MAIN_H"
-#define "MAIN_H"
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdlib.h>
 #include <stdio.h>
-void _putchar(char *c)
-{
-	return (write(1, &c, 1));
-}
-void _puts_recursion(char *s);
 
+int _putchar(char c);
+
+void _puts_recursion(char *s);
 
 #endif
