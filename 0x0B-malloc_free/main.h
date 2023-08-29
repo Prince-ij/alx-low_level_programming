290d7256e39bbf2fef57fe52a9429d52d3ef1360
Prince-ij <aminuibrahimtbefifth@gmail.com>
2023-08-29 15:30:48 -0700

    gy

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -7,12 +7,6 @@
 int _putchar(char *c);
 
 char *create_array(unsigned int size, char c);
-
-
-
-#ifndef MAIN_H
-#define MAIN_H
-
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
