3641fd3d94dd8be2de3b4b1057c0b9360b21e025
Prince-ij <aminuibrahimtbefifth@gmail.com>
2023-11-09 01:48:25 -0800

    risky move

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
- * _isupper - main function
- * @c: param
- * Return: void
+ * _isupper - function to check for uppercase chaaracter
+ *
+ * @c: character to check
+ *
+ * Return: 1 if true , 0 otherwise
  */
-int _isupper(int c)
+int _isupper(char c)
 {
-	if (c >= 65 && c <= 90)
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,20 +1,22 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+
 #include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
 
-void print_number(int n);
-void print_triangle(int size);
-void print_square(int size);
-void print_diagonal(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
 void print_line(int n);
 void more_numbers(void);
-void print_most_numbers(void);
-void print_numbers(void);
-int mul(int a, int b);
-int _isdigit(int c);
-int _isupper(int c);
-
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+int _putchar(char c);
 
 #endif
