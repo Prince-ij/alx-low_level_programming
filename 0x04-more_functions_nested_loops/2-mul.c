ec01fb767714abeb373d4590032c983ed689b3a4
Prince-ij <aminuibrahimtbefifth@gmail.com>
2023-11-09 02:32:49 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - Function to multiply two arbitarary integers
+ *
+ * @a: First param
+ * @b: Secon param
+ *
+ * Return: a*b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
