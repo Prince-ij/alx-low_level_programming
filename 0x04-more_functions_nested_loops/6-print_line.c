678ed51b6506de234c88fe3f25aa2b4e09d7214e
Prince-ij <aminuibrahimtbefifth@gmail.com>
2023-11-09 05:51:45 -0800

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - print a [_] to the stdout
+ *
+ * @n: number of [_] to be printed
+ * Return: void
+ */
+void print_line(int n)
+{
+	while (n)
+	{
+		if (n == 0)
+			_putchar('\n');
+		else if (n < 0)
+			_putchar('\n');
+		else
+		{
+			_putchar('_');
+			n--;
+		}
+	}
+}
