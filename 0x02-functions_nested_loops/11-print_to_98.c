64641ac0179e907d15181122d5650447b055d02f
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-02-19 10:48:16 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,7 +6,7 @@
  */
 void print_to_98(int n)
 {
-	while (n <= 98)
+	while (n < 98)
 	{
 	if (n != 98)
 	{
@@ -15,6 +15,19 @@ void print_to_98(int n)
 	else
 		printf("%d", n);
 	n++;
+	}
+	
+	if ( n == 98)
+		printf("%d",n);
+	while (n > 98)
+	{
+	if (n != 98)
+	{
+		printf("%d, ", n);
+	}
+	else
+		printf("%d", n);
+	n--;
 	}
 	putchar('\n');
 }
