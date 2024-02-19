c7ebf0c6970e43fb563f7b527666236d5a895221
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-02-19 09:31:50 -0800

    9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,14 +8,23 @@ void times_table(void)
 {
 	int i, j, r;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j < 9; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			r = i * j;
-			_putchar(r);
-			if (j < 8)
+			if (r > 9)
+			{
+				_putchar('0' + r);
+				_putchar('0' + r % 10);
+			}
+			else
+				_putchar('0' + r);
+			if (j != 9)
+			{
 				_putchar(',');
+				_putchar(' ');
+			}
 		}
 		_putchar('\n');
 	}
