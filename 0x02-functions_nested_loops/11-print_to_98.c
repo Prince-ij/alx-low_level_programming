f2727cb07ce58333f33066ce8cfb899eda9bebf2
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-02-19 10:34:46 -0800

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_to_98 - print numbers
+ * @n: int param
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	while (n <= 98)
+	{
+	if (n != 98)
+	{
+		printf("%d, ", n);
+	}
+	else
+		printf("%d", n);
+	n++;
+	}
+	putchar('\n');
+}
