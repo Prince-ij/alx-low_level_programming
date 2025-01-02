56bf39e7d4970b62578174b25698fa57fb491bf3
root <root@LAPTOP-56RVIP6N.localdomain>
2025-01-02 18:04:08 +0100

    last

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,13 +11,18 @@ int print_last_digit(int n)
 {
 	if (n > 1)
 	{
+		_putchar(n % 10);
 		return (n % 10);
 	}
 	else if (n < 1)
 	{
 		n *= -1;
+		_putchar(-(n % 10));
 		return (-(n % 10));
 	}
 	else
+	{
+		_putchar(0);
 		return (0);
+	}
 }
