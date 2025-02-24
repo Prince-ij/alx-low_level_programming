6a6cacd842c39f924e3cd65531673628d5f9af88
root <root@LAPTOP-56RVIP6N.localdomain>
2025-02-24 06:30:50 +0100

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,22 +15,27 @@ void times_table(void)
 			res = i * j;
 			if (res > 9)
 			{
+				_putchar(' ');
 				_putchar('0' + res / 10);
 				_putchar('0' + res % 10);
 				if (j != 9)
 				{
 					_putchar(',');
-					_putchar(' ');
 				}
+			}
+			else if (j == 0)
+			{
+				_putchar('0' + res);
+				_putchar(',');
 			}
 			else
 			{
+				_putchar(' ');
+				_putchar(' ');
 				_putchar('0' + res);
 				if (j != 9)
 				{
 					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
 				}
 			}
 		}
