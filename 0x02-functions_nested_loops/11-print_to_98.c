9e096caccf159b7b156825ba378f338e639e1c92
root <root@LAPTOP-56RVIP6N.localdomain>
2025-02-24 06:54:14 +0100

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,32 +6,33 @@
  */
 void print_to_98(int n)
 {
+	if (n >= 98)
+	{
+	while (n >= 98)
+	{
+		if (n == 98)
+		{
+			printf("%d\n", n);
+			break;
+		}
+		else {
+			printf("%d, ", n);
+		}
+		n--;
+	}
+	}
+	else 
+	{
 	while (n <= 98)
 	{
-		if (n < 0)
-		{
-			printf("%d", n);
-		}
-		if (n < 10)
-			_putchar('0' + n);
-		else
-		{
-			_putchar('0' + n / 10);
-			_putchar('0' + n % 10);
-		}
 		if (n == 98)
-			break;
-		_putchar(',');
-		_putchar(' ');
+		{
+			printf("%d\n", n);
+		}
+		else {
+			printf("%d, ", n);
+		}
 		n++;
 	}
-	while (n > 98)
-	{
-		 _putchar('0' + n / 10);
-		 _putchar('0' + n % 10);
-		 _putchar(',');
-		 _putchar(' ');
-		 n--;
 	}
-	_putchar('\n');
 }
