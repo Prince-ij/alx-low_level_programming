03121912f9a96ce645182903ff023e63951d37a3
root <root@LAPTOP-56RVIP6N.localdomain>
2025-02-28 07:19:57 +0100

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - func name
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 14; i++)
+	{
+		for (i = 0; i < 10; i++)
+		{
+			_putchar('0' + i);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
