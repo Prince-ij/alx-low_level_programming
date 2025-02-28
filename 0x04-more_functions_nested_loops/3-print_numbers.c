dafa0499eea39bc7bb8fbd6f3bd814b506d22869
root <root@LAPTOP-56RVIP6N.localdomain>
2025-02-28 07:09:58 +0100

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - func name
+ * Return: void
+ */
+void print_numbers(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+}
