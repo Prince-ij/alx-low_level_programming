a02a6271706c087a906fe41aed5cbaa30aaab36a
root <root@LAPTOP-56RVIP6N.localdomain>
2025-02-28 07:21:10 +0100

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,9 +9,9 @@ void more_numbers(void)
 
 	for (i = 0; i < 14; i++)
 	{
-		for (i = 0; i < 10; i++)
+		for (j = 0; j < 10; j++)
 		{
-			_putchar('0' + i);
+			_putchar('0' + j);
 		}
 		_putchar('\n');
 	}
